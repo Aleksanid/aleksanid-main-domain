34dafb9f5456a368a8f6f781c1aad170459d3e19
Aleksanid <omegison@gmail.com>
2022-05-14 19:31:55 +0300

    Initial commit

---
+++ src/main/java/ua/aleksanid/maindomain/MainDomainApplication.java
@@ -0,0 +1,13 @@
+package ua.aleksanid.maindomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainDomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/aleksanid/maindomain/MainDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.aleksanid.maindomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
