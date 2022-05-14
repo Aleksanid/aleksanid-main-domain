72fe4e6fa5add204e0856b1b3a890541c622385f
Aleksanid <omegison@gmail.com>
2022-05-14 20:29:01 +0300

    Setup first controller and database.

---
+++ src/main/java/ua/aleksanid/maindomain/controllers/ProductsController.java
@@ -0,0 +1,17 @@
+package ua.aleksanid.maindomain.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.aleksanid.maindomain.models.Product;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController("/product")
+public class ProductsController {
+
+    @GetMapping("/")
+    public List<Product> getProducts(){
+        return Collections.singletonList(new Product(1L,"Test","Test product"));
+    }
+}
---
+++ src/main/java/ua/aleksanid/maindomain/models/Product.java
@@ -0,0 +1,37 @@
+package ua.aleksanid.maindomain.models;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(description, product.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
+}
